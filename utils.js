ef9f2a80cbbfe1e5416c13213501dc4cc078953d
bay <bayeldzhoroev@gmail.com>
2024-09-26 23:30:45 +0300

    Fix styling

--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
 const parseXML = (xmlData) => {
-    const parser = new DOMParser;
-    const doc = parser.parseFromString(xmlData, "text/xml");
-    const errorNode = doc.querySelector('parsererror');
-    if (errorNode) {
-      throw new Error('notRSS')
-    } else {
-      return doc;
-    }
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(xmlData, 'text/xml');
+  const errorNode = doc.querySelector('parsererror');
+  if (errorNode) {
+    throw new Error('notRSS');
+  } else {
+    return doc;
+  }
 };
 
-export { parseXML }; 
+export default parseXML;
