a344542a289d17e9a01f5d015fa9fdff434746e2
bay <bayeldzhoroev@gmail.com>
2024-09-27 21:05:32 +0300

    Refactor view.js

--- src/view.js
+++ src/view.js
@@ -1,5 +1,27 @@
 import onChange from 'on-change';
 
+const createPostButton = (id, t) => {
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+  button.setAttribute('type', 'button');
+  button.setAttribute('data-id', id);
+  button.setAttribute('data-bs-toggle', 'modal');
+  button.setAttribute('data-bs-target', '#modal');
+  button.textContent = t('viewPostButton');
+  return button;
+};
+
+const createPostLink = (url, id, title) => {
+  const link = document.createElement('a');
+  link.setAttribute('href', url);
+  link.setAttribute('data-id', id);
+  link.setAttribute('target', '_blank');
+  link.setAttribute('rel', 'noopener noreferrer');
+  link.classList.add('fw-bold');
+  link.textContent = title;
+  return link;
+};
+
 const createPostElems = (posts, t) => posts.map((post) => {
   const postCard = document.createElement('li');
   postCard.classList.add(
@@ -10,24 +32,9 @@ const createPostElems = (posts, t) => posts.map((post) => {
     'border-0',
     'border-end-0',
   );
-
-  const link = document.createElement('a');
-  link.setAttribute('href', post.link);
-  link.setAttribute('data-id', post.id);
-  link.setAttribute('target', '_blank');
-  link.setAttribute('rel', 'noopener noreferrer');
-  link.classList.add('fw-bold');
-  link.textContent = post.title;
-  postCard.append(link);
-
-  const button = document.createElement('button');
-  button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-  button.setAttribute('type', 'button');
-  button.setAttribute('data-id', post.id);
-  button.setAttribute('data-bs-toggle', 'modal');
-  button.setAttribute('data-bs-target', '#modal');
-  button.textContent = t('viewPostButton');
-  postCard.append(button);
+  const link = createPostLink(post.link, post.id, post.title);
+  const button = createPostButton(post.id, t);
+  postCard.append(link, button);
 
   return postCard;
 });
@@ -61,7 +68,7 @@ const renderNewPosts = (state, t, elements) => {
   }
 };
 
-const handleModal = (state, activeId) => {
+const renderModal = (state, activeId) => {
   const modalWindow = document.querySelector('.modal-dialog');
   const activePost = state.posts.find((post) => post.id === activeId);
   modalWindow.querySelector('.modal-title')
@@ -70,48 +77,76 @@ const handleModal = (state, activeId) => {
     .textContent = activePost.description;
 };
 
-const handleProcessError = (elems, error, t) => {
+const renderProcessError = (elems, error, t) => {
   elems.feedback.classList.add('text-danger');
   elems.feedback.textContent = t(`loadingStates.${error}`);
   elems.formInput.removeAttribute('readonly');
   elems.formSubmit.removeAttribute('disabled');
 };
 
-const handleFilling = (error, elems, t) => {
+const renderFillingState = (error, elems, t) => {
   if (error) {
-    handleProcessError(elems, error, t);
+    renderProcessError(elems, error, t);
   }
   elems.formInput.removeAttribute('readonly');
   elems.formSubmit.removeAttribute('disabled');
 };
 
-const handleFormError = (elems, error, t) => {
+const renderFormError = (elems, error, t) => {
   elems.feedback.classList.add('text-danger');
   elems.formInput.classList.add('is-invalid');
   elems.feedback.textContent = t(`formErrors.${error}`);
 };
 
-const handleFormValid = (elems) => {
+const renderFormValid = (elems) => {
   if (elems.formInput.classList.contains('is-invalid')) {
     elems.formInput.classList.remove('is-invalid');
   }
   elems.feedback.textContent = '';
 };
 
-const handleSending = (elems) => {
+const renderSendingState = (elems) => {
   clearFeedback(elems);
   elems.feedback.textContent = '';
   elems.formInput.setAttribute('readonly', true);
   elems.formSubmit.setAttribute('disabled', true);
 };
 
-const handleFinished = (elems, state, t) => {
-  elems.feedback.classList.add('text-success');
-  elems.feedback.textContent = t('loadingStates.finished');
-  elems.formInput.removeAttribute('readonly');
-  elems.formSubmit.removeAttribute('disabled');
+const createFeedElem = (feed) => {
+  const feedCard = document.createElement('li');
+  feedCard.classList.add('list-group-item', 'border-0', 'border-end-0');
+  const feedCardTitle = document.createElement('h3');
+  feedCardTitle.classList.add('h6', 'm-0');
+  feedCardTitle.textContent = feed.feedTitle;
+  const feedCardDesc = document.createElement('p');
+  feedCardDesc.classList.add('m-0', 'small', 'text-black-50');
+  feedCardDesc.textContent = feed.description;
 
-  elems.postsContainer.innerHTML = '';
+  feedCard.append(feedCardTitle, feedCardDesc);
+  return feedCard;
+};
+
+const createFeeds = (state, elems, t) => {
+  elems.feedsContainer.innerHTML = '';
+  const feedHolder = document.createElement('div');
+  feedHolder.classList.add('card', 'border-0');
+
+  const feedTitleBox = document.createElement('div');
+  feedTitleBox.classList.add('card-body');
+  const feedTitle = document.createElement('h2');
+  feedTitle.classList.add('card-title', 'h4');
+  feedTitle.textContent = t('feedsTitle');
+  feedTitleBox.append(feedTitle);
+
+  const feedsUl = document.createElement('ul');
+  feedsUl.classList.add('list-group', 'border-0', 'rounded-0');
+  const feeds = state.feeds.map(createFeedElem);
+  feedsUl.append(...feeds);
+  feedHolder.append(feedTitleBox, feedsUl);
+  return feedHolder;
+};
+
+const createPostList = (postElems, t) => {
   const postHolder = document.createElement('div');
   postHolder.classList.add('card', 'border-0');
 
@@ -126,69 +161,59 @@ const handleFinished = (elems, state, t) => {
   const postUl = document.createElement('ul');
   postUl.classList.add('list-group', 'border-0', 'rounded-0');
 
-  const posts = createPostElems(state.posts, t);
-
-  postUl.append(...posts);
+  postUl.append(...postElems);
   postHolder.append(postTitleBox, postUl);
+  return postHolder;
+};
+
+const renderFinishedState = (elems, state, t) => {
+  elems.feedback.classList.add('text-success');
+  elems.feedback.textContent = t('loadingStates.finished');
+  elems.formInput.removeAttribute('readonly');
+  elems.formSubmit.removeAttribute('disabled');
+
+  const postsElems = createPostElems(state.posts, t);
+  const postHolder = createPostList(postsElems, t);
+  elems.postsContainer.innerHTML = '';
   elems.postsContainer.append(postHolder);
   renderSeenPosts(state);
 
-  elems.feedsContainer.innerHTML = '';
-  const feedHolder = document.createElement('div');
-  feedHolder.classList.add('card', 'border-0');
-
-  const feedTitleBox = document.createElement('div');
-  feedTitleBox.classList.add('card-body');
-  const feedTitle = document.createElement('h2');
-  feedTitle.classList.add('card-title', 'h4');
-  feedTitle.textContent = t('feedsTitle');
-  feedTitleBox.append(feedTitle);
-
-  const feedsUl = document.createElement('ul');
-  feedsUl.classList.add('list-group', 'border-0', 'rounded-0');
-  const feeds = state.feeds.map((feed) => {
-    const feedCard = document.createElement('li');
-    feedCard.classList.add('list-group-item', 'border-0', 'border-end-0');
-    const feedCardTitle = document.createElement('h3');
-    feedCardTitle.classList.add('h6', 'm-0');
-    feedCardTitle.textContent = feed.feedTitle;
-    const feedCardDesc = document.createElement('p');
-    feedCardDesc.classList.add('m-0', 'small', 'text-black-50');
-    feedCardDesc.textContent = feed.description;
-
-    feedCard.append(feedCardTitle, feedCardDesc);
-    return feedCard;
-  });
-  feedsUl.append(...feeds);
-
-  feedHolder.append(feedTitleBox, feedsUl);
+  const feedHolder = createFeeds(state, elems, t);
   elems.feedsContainer.append(feedHolder);
+};
+
+const handleForm = (elems, value, t) => {
+  if (!value.isValid) {
+    renderFormError(elems, value.error, t);
+  } else {
+    renderFormValid(elems);
+  }
+};
+
+const handleLoadingState = (value, elems, state, t) => {
+  switch (value.state) {
+    case 'sending':
+      renderSendingState(elems);
+      break;
+    case 'finished':
+      renderFinishedState(elems, state, t);
+      break;
+    case 'filling':
+      renderFillingState(value.error, elems, t);
+      break;
+    default:
+      break;
+  }
 };
 
 const watch = (state, elements, { t }) => {
   const watchedState = onChange(state, (path, value) => {
     switch (path) {
       case 'form':
-        if (!value.isValid) {
-          handleFormError(elements, value.error, t);
-        } else {
-          handleFormValid(elements);
-        }
+        handleForm(elements, value, t);
         break;
       case 'loadingProcess':
-        switch (value.state) {
-          case 'sending':
-            handleSending(elements);
-            break;
-          case 'finished':
-            handleFinished(elements, state, t);
-            break;
-          case 'filling':
-            handleFilling(value.error, elements, t);
-            break;
-          default:
-            break;
-        }
+        handleLoadingState(value, elements, state, t);
         break;
       case 'posts':
         renderNewPosts(state, t, elements);
@@ -197,13 +222,12 @@ const watch = (state, elements, { t }) => {
         renderSeenPosts(state);
         break;
       case 'ui.activeModalId':
-        handleModal(state, value);
+        renderModal(state, value);
         break;
       default:
         break;
     }
   });
-
   return watchedState;
 };
 
