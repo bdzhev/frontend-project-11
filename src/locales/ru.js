d159d6579b74ad8c43cc331c7437577adb86a90b
bay <bayeldzhoroev@gmail.com>
2024-09-27 20:26:38 +0300

    Update russian locales

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,5 +1,19 @@
 export default {
   translation: {
     title: 'RSS аггрегатор',
+    formErrors: {
+      notURL: 'Ссылка должна быть валидным URL',
+      alreadyExists: 'RSS уже существует',
+      emptyField: 'Не должно быть пустым',
+
+    },
+    loadingStates: {
+      finished: 'RSS успешно загружен',
+      networkErr: 'Ошибка сети',
+      notRSS: 'Ресурс не содержит валидный RSS',
+    },
+    viewPostButton: 'Просмотр',
+    feedsTitle: 'Фиды',
+    postsTitle: 'Посты',
   },
 };
