f50d9326a19071bfda2b7760a97f99402b39dbbd
bay <bayeldzhoroev@gmail.com>
2024-09-30 15:46:42 +0300

    Fix locale packaging

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -4,15 +4,14 @@ export default {
       notURL: 'Ссылка должна быть валидным URL',
       alreadyExists: 'RSS уже существует',
       emptyField: 'Не должно быть пустым',
+      notRSS: 'Ресурс не содержит валидный RSS',
     },
     loadingStates: {
       finished: 'RSS успешно загружен',
       networkErr: 'Ошибка сети',
-      notRSS: 'Ресурс не содержит валидный RSS',
     },
     viewPostButton: 'Просмотр',
     feedsTitle: 'Фиды',
     postsTitle: 'Посты',
   },
 };
-
